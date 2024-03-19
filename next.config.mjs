3496b2ed415138ea915e40e1a6cc775c6b28c90d
Ramadhan <ramiqcom@gmail.com>
2024-03-19 17:46:23 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import CopyPlugin from 'copy-webpack-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          { from: './node_modules/gdal3.js/dist/package/gdal3WebAssembly.wasm', to: 'static' },
+          { from: './node_modules/gdal3.js/dist/package/gdal3WebAssembly.data', to: 'static' },
+        ],
+      }),
+		);
+
+		return config;
+  },
+};
 
 export default nextConfig;
