1c8c9e98d24a7d43688a2d72771cf7a0b37e5d04
Ramadhan <ramiqcom@gmail.com>
2024-07-11 13:07:41 +0200

    fix package

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    const externals = ['gdal-async'];
+    if (config.externals) config.externals.push(...externals);
+    else config.externals = externals;
+    return config;
+  },
+};
 
 export default nextConfig;
